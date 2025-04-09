c7749036724b1e7e2aa0d026cfcd7e2e3359fe86
0xdmtry <0xdmtry@gmail.com>
2025-04-08 23:07:56 -0400

    Migration content

--- app/authorizer/migration/src/m20250407_184459_create_user_and_passcode.rs
+++ app/authorizer/migration/src/m20250407_184459_create_user_and_passcode.rs
@@ -6,36 +6,71 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(User::Id).uuid().not_null().primary_key())
+                    .col(ColumnDef::new(User::Email).string_len(254).not_null().unique_key())
+                    .col(ColumnDef::new(User::CreatedAt).timestamp().no_null())
+                    .col(ColumnDef::new(User::UpdatedAt).timestamp().not_null())
+                    .col(ColumnDef::new(User::DeletedAt).timestamp().null())
+                    .to_owned(),
+            )
+            .await?;
 
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Passcode::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(ColumnDef::new(Passcode::Id).uuid().not_null().primary_key())
+                    .col(ColumnDef::new(Passcode::UserId).uuid().not_null())
+                    .col(ColumnDef::new(Passcode::Code).string_len(16).not_null())
+                    .col(ColumnDef::new(Passcode::ExpiredAt).timestamp().not_null())
+                    .col(ColumnDef::new(Passcode::Used).boolean().not_null())
+                    .col(ColumnDef::new(Passcode::CreatedAt).timestamp().not_null())
+                    .col(ColumnDef::new(Passcode::UpdatedAt).timestamp().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-passcode-user_id")
+                            .from(Passcode::Table, Passcode::UserId)
+                            .to(User::Table, User::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
                     .to_owned(),
             )
             .await
+
+        
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
-        manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+        manager.drop_table(Table::drop().table(Passcode::Table).to_owned()).await?;
+        manager.drop_table(Table::drop().table(User::Table).to_owned()).await
     }
 }
 
-#[derive(DeriveIden)]
-enum Post {
+#[derive(Iden)]
+enum User {
     Table,
     Id,
-    Title,
-    Text,
+    Email,
+    CreatedAt,
+    UpdatedAt,
+    DeletedAt,
+}
+
+#[derive(Iden)]
+enum Passcode {
+    Table,
+    Id,
+    UserId,
+    Code,
+    ExpiredAt,
+    Used,
+    CreatedAt,
+    UpdatedAt,
 }
