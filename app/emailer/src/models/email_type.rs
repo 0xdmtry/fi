fc66cbeb5451249f1332cc4298278b729b93058a
0xdmtry <0xdmtry@gmail.com>
2025-04-10 19:58:05 -0400

    Fix for the enums

--- app/emailer/src/models/email_type.rs
+++ app/emailer/src/models/email_type.rs
@@ -1,7 +1,7 @@
 use sea_orm::entity::prelude::*;
 
 #[derive(Debug, Clone, PartialEq, Eq, EnumIter, DeriveActiveEnum)]
-#[sea_orm(rs_type="String", db_type="String")]
+#[sea_orm(rs_type="String", db_type="Text")]
 pub enum EmailType {
     #[sea_orm(string_value="passcode")]
     Passcode,
